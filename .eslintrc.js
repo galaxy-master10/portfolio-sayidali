38bb307a3ad382f659bc56123b2a47e209f80b88
sayidali <sayidali.jama@student.howest.be>
2025-05-29 13:04:10 +0200

    eslint disablement files

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+    root: true,
+    rules: {
+        // Disable all rules
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        // Add more rules here if needed
+    },
+}
